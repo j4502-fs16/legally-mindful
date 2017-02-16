d7286af0180e847f3aecf866ab4bb8eeea5a50e9
Jen Para <jmpxt4@mail.missouri.edu>
2017-02-16 12:07:49 -0600

    copying and pasting old docs

---
+++ dropdownjava.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+  $('.menu-trigger').click(function() {
+    $('nav ul').slideToggle(600);
+});
+
+$(window).resize(function(){
+if (   $(window).width() > 600 ){
+  $('nav ul').removeAttr('style');
+}
+});
+});
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    for (var d = 0; d < dropdowns.length; d++) {
+      var openDropdown = dropdowns[d];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
